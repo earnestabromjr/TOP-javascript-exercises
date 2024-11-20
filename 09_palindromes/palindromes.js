82ebd2efdaff9e7e02aa7c973ffca0329cbba430
Earnest Abrom <156559830+EAbromjr@users.noreply.github.com>
2024-11-20 17:06:26 -0600

    Completed palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanedWord = word
+        .toLowerCase()
+        .split('')
+        .filter((char) => alphanumerical.includes(char))
+        .join('');
+    const newWord = cleanedWord.split('').reverse().join('');
 
+    return cleanedWord === newWord;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
