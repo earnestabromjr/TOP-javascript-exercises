a62f8d422a8c6520c4e8a92de119703c6607c202
earnestabromjr <abromearnest@gmail.com>
2024-11-20 13:59:37 -0600

    Init Commit

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return (a) - (b)
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce(((total, num) => total + num), 0);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce(((total, num) => total * num));
+};
+
+const power = function(a, b) {
+  return Math.pow(a, b);
 
 };
 
-const power = function() {
-	
-};
+const factorial = function(num) {
+  if (num < 0) return undefined;
+  if (num === 0 || num ===1) return 1;
 
-const factorial = function() {
-	
+  let result = 1;
+  for (let i = 2; i <= num; i++) {
+    result *= i;
+  }
+  return result
+
 };
 
 // Do not edit below this line
