89962199d6261b12f17d3f7e60bb4eca52a9ad74
Earnest Abrom <156559830+EAbromjr@users.noreply.github.com>
2024-11-20 19:51:59 -0600

    Completed fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (typeof num === !'number' || num < 0) {
+        return 'OOPS';
+    }
+    let count = 0;
+    const sequence = [0, 1]
+    let i = 2;
 
+    while (count <= num) {
+        sequence[i] = sequence[i - 1] + sequence[i - 2];
+        i++
+        count++;
+    }
+    // console.log(sequence[num - 1])
+    // console.log(num);
+    // console.log(count);
+    return sequence[num];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
